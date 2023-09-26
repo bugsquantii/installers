730a05639d75376845dab8215100a1ff83014347
noobiiii <noobiiii@users.noreply.github.com>
2023-09-26 18:58:19 +0200

    update

---
+++ debian_sid_installs/install_appimage_mqttexplorer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p ~/AppImages
+
+wget -O ~/AppImages/mqttExplorer.AppImage 'https://github.com/thomasnordquist/MQTT-Explorer/releases/download/0.0.0-0.4.0-beta1/MQTT-Explorer-0.4.0-beta1.AppImage'
+
+chmod +x ~/AppImages/mqttExplorer.AppImage
+
+#Download icon
+mkdir -p ~/AppImages/icons
+wget -O ~/AppImages/icons/mqttExplorer.png 'https://github.com/thomasnordquist/MQTT-Explorer/blob/master/res/icon.png?raw=true'
+
+#Create desktop entry
+cat > ~/.local/share/applications/mqttExplorer.desktop <<EOL
+[Desktop Entry]
+Name=MQTT-Explorer
+Exec=/home/$(whoami)/AppImages/mqttExplorer.AppImage
+Icon=/home/$(whoami)/AppImages/icons/mqttExplorer.png
+Terminal=false
+Type=Application
+Categories=Office;
+EOL
+
